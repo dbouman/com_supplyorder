dd5df5c39d5ab5e1b8c1329b3995300e2b85ecdf
Danny Bouman <dannybb@gmail.com>
2012-08-14 17:17:47 -0400

    Fixed delete button again to stay on same page

--- component/views/list/tmpl/approved.php
+++ component/views/list/tmpl/approved.php
@@ -60,7 +60,7 @@ if(isset($this->message)){
 				<td><?php echo $request['request_cost']; ?></td>
 				<td><?php echo $request['status_name']; ?></td>
 				<td><a class="popup" href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=details&tmpl=component&request_id=' . $request['request_id'] ); ?>">Details</a></td>
-				<td><a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=saved&task=delete_request&request_id=' . $request['request_id'] ); ?>">Delete</a></td>
+				<td><a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=approved&task=delete_request&request_id=' . $request['request_id'] ); ?>">Delete</a></td>
 			</tr>
 		<?php
 			} 
